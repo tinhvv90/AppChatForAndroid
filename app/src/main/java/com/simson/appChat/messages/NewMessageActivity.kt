ab1559a1d802f40af0cbb48b563d500a80894f22
TinhVV <tinhvv90@gmail.com>
2019-05-24 16:53:54 +0700

    check not am i.

--- app/src/main/java/com/simson/appChat/messages/NewMessageActivity.kt
+++ app/src/main/java/com/simson/appChat/messages/NewMessageActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -47,7 +48,7 @@ class NewMessageActivity : AppCompatActivity() {
                     Log.d("NewMessage", it.toString())
 
                     val user = it.getValue(User::class.java)
-                    if (user != null) {
+                    if (user != null && user.uid != FirebaseAuth.getInstance().uid) {
                         adapter.add(UserItem(user))
                     }
                 }
